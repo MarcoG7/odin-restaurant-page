4eda571fcac1263e42bb4428eba912da8b1dff95
Marc <63096791+MarcoG7@users.noreply.github.com>
2025-08-30 13:49:07 +0300

    Change page content by clicking buttons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { home_page_content } from "./home_page.js";
+import { home_page_content } from "./pages/home_page.js";
+import { about_page_content } from "./pages/about_page.js";
+
+const home_button = document.getElementById("home-button");
+const about_button = document.getElementById("about-button");
 
 const container = document.getElementById("container");
 
 container.appendChild(home_page_content);
+
+home_button.addEventListener("click", () => {
+    container.innerHTML = "";
+    container.appendChild(home_page_content);
+});
+about_button.addEventListener("click", () => {
+    container.innerHTML = "";
+    container.appendChild(about_page_content);
+});
