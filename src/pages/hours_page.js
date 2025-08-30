917337d5316c3be0e49cf5f8ceba1888aad518ba
Marc <63096791+MarcoG7@users.noreply.github.com>
2025-08-30 16:28:58 +0300

    Add hours content

---
+++ src/pages/hours_page.js
@@ -0,0 +1,34 @@
+export { hours_page_content };
+
+const hours_page_content = document.createElement("div");
+
+const restaurant_name = document.createElement("h1");
+restaurant_name.textContent = "Ember & Ash";
+restaurant_name.classList.add("retaurant-name");
+
+const restaurant_tagline = document.createElement("h3");
+restaurant_tagline.textContent = "Wood-Fired Flavors, Modern Comfort";
+restaurant_tagline.classList.add("restaurant-tagline");
+
+const hours_list = document.createElement("ul");
+hours_list.classList.add("hours-list");
+
+const hours_item_MTWT = document.createElement("li");
+hours_item_MTWT.textContent = "Monday - Thursday: 11:30 AM - 9:00 PM";
+hours_item_MTWT.classList.add("hours-item");
+
+const hours_item_FS = document.createElement("li");
+hours_item_FS.textContent = "Friday - Saturday: 11:30 AM - 10:30 PM";
+hours_item_FS.classList.add("hours-item");
+
+const hours_item_S = document.createElement("li");
+hours_item_S.textContent = "Sunday: 10:00 AM - 8:00 PM (Brunch: 10 AM - 2 PM)";
+hours_item_S.classList.add("hours-item");
+
+hours_list.appendChild(hours_item_MTWT);
+hours_list.appendChild(hours_item_FS);
+hours_list.appendChild(hours_item_S);
+
+hours_page_content.appendChild(restaurant_name);
+hours_page_content.appendChild(restaurant_tagline);
+hours_page_content.appendChild(hours_list);
